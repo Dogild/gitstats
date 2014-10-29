623adc41c7f7021de42226dcbd1fc926a2894971
Alexandre Wilhelm <contact.dogild@gmail.com>
2014-10-29 16:46:43 -0700

    Added setup, should not work now....

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='gitstats',
+      version='0.0.1',
+      description='Python library to retrieve commits statistics from github',
+      author='Alexandre Wilhelm',
+      author_email='alexandre.wilhelmfr@gmail.com',
+      url='https://github.com/Dogild/gitstats'
+     )

2fd432fa989a18bd70a7bebcf2ee1903d441ef84
Alexandre Wilhelm <contact.dogild@gmail.com>
2014-11-09 23:56:31 -0800

    Changed API

--- example.py
+++ example.py
@@ -3,26 +3,10 @@
 from gitstats.models.account import Account
 from gitstats.models.commit import Commit
 
-account = Account("primalmotion")
-repositories = list()
-forks = list()
-contributed_forks = list()
+account = Account("Dogild")
 
 print "Let's take a look to the account %s" % account.username
-
-print "Retrieving the repositories"
-repositories = account.get_repositories()
-print repositories
-
-print "Retrieving commits for each repositories"
-commits = account.get_commits()
-print commits
-print "Number of commits %s" % len(commits)
-
-print "Retrieving issues for each repositories"
-issues = account.get_issues()
-print issues
-print "Number of issues %s" % len(issues)
-
-print "Total contributions for %s" % account.username
-print account.contributions
+print "Retrieving contributions"
+contributions = account.get_contributions_of_last_year()
+print contributions
+print "Totale number of contributions %s" % len(contributions)
--- gitstats/models/account.py
+++ gitstats/models/account.py
@@ -10,20 +10,44 @@ from gitstats.lib.routes import make_uri_user, make_uri_search_issue, make_uri_o
 
 class Account(object):
 
-    def __init__(self, username, repositories=list(), forks=list(), contributions=0, end_date=datetime.datetime.today()):
+    def __init__(self, username):
         self.username = username
-        self.repositories = repositories
-        self.forks = forks
-        self.contributions = contributions
-        self.end_date = end_date
-        self.start_date = end_date - datetime.timedelta(days=365)
+        self.repositories = list()
+        self.forks = list()
+        self.contributions = 0
+        self.end_date = datetime.datetime.today()
+        self.start_date = self.end_date - datetime.timedelta(days=365)
         self.github_connection = GithubConnection(username)
 
         dict_user = self.github_connection.get(make_uri_user(username))
         self.repos_url = dict_user["repos_url"]
         self.orgs = self.github_connection.get(make_uri_orgs(username))
+        self.repositories = self._get_repositories()
 
-    def get_repositories(self):
+    def get_contributions_of_last_year(self):
+        self.end_date = datetime.datetime.today()
+        self.start_date = self.end_date - datetime.timedelta(days=365)
+
+        return self._get_contributions()
+
+    def get_contributions_for_dates(self, start_date, end_date):
+        self.end_date = end_date
+        self.start_date = start_date
+
+        return self._get_contributions()
+
+    def _get_contributions(self):
+        self.contributions = 0
+        contributions_list = list()
+        commits = self._get_commits(self.start_date, self.end_date)
+        issues = self._get_issues(self.start_date, self.end_date)
+
+        contributions_list.extend(commits)
+        contributions_list.extend(issues)
+
+        return contributions_list
+
+    def _get_repositories(self):
 
         params = dict()
         params["type"] = "all"
@@ -51,47 +75,49 @@ class Account(object):
             if new_repository not in repositories:
                 repositories.append(new_repository)
 
-        self.repositories = repositories
+        return repositories
 
-        return self.repositories
-
-    def get_commits(self):
+    def _get_commits_for_repository(self, repository, start_date, end_date):
 
         params = dict()
         params["author"] = self.username
-        params["since"] = self.start_date.isoformat()
-        params["until"] = self.end_date.isoformat()
+        params["since"] = start_date.isoformat()
+        params["until"] = end_date.isoformat()
+
+        commits = list()
+
+        new_commits = self.github_connection.get(repository.commits_url, params)
+
+        for new_commit in new_commits:
+            date = datetime.datetime.strptime(new_commit["commit"]["author"]["date"], "%Y-%m-%dT%H:%M:%SZ")
+            commit = Commit(date=date, sha=new_commit["sha"], author=new_commit["commit"]["author"]["name"], message=new_commit["commit"]["message"])
+            commits.append(commit)
+
+        return commits
+
+    def _get_commits(self, start_date, end_date):
 
         commits = list()
 
         for repository in self.repositories:
-            new_commits = self.github_connection.get(repository.commits_url, params)
-
-            for new_commit in new_commits:
-                date = datetime.datetime.strptime(new_commit["commit"]["author"]["date"], "%Y-%m-%dT%H:%M:%SZ")
-                commit = Commit(date=date, sha=new_commit["sha"], author=new_commit["commit"]["author"]["name"], message=new_commit["commit"]["message"])
-                commits.append(commit)
-
-        self.contributions += len(commits)
+            commits.extend(self._get_commits_for_repository(repository, start_date, end_date))
 
         return commits
 
-    def get_issues(self):
+    def _get_issues(self, start_date, end_date):
 
         params = dict()
         params["q"] = "author:%s" % self.username
 
         issues = list()
 
-        dict_issues = self.github_connection.search_issues(uri=make_uri_search_issue(), params=params, min_date=self.start_date)
+        dict_issues = self.github_connection.search_issues(uri=make_uri_search_issue(), params=params, min_date=start_date)
 
         for dict_issue in dict_issues:
             date = datetime.datetime.strptime(dict_issue["created_at"], "%Y-%m-%dT%H:%M:%SZ")
             issue = Issue(date=date, author=dict_issue["user"]["login"], number=dict_issue["number"], title=dict_issue["title"])
 
-            if date > self.start_date:
+            if date > start_date:
                 issues.append(issue)
-
-        self.contributions +=len(issues)
 
         return issues

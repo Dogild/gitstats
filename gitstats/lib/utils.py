9b31ee84d528ddc50b9b0006b41cb67949b7242a
Alexandre Wilhelm <contact.dogild@gmail.com>
2014-11-17 23:41:13 -0800

    Added comment for help...

--- gitstats/lib/utils.py
+++ gitstats/lib/utils.py
@@ -10,6 +10,7 @@ token = os.environ["GITSTATS_TOKEN"]
 def make_headers():
     headers = dict()
     headers["Authorization"] = "Basic %s" % (urlsafe_b64encode("%s:x-oauth-basic" % token))
+    #headers["Authorization"] = "Basic %s" % (urlsafe_b64encode("login:pwd"))
     headers["Content-Type"] = "application/json"
     headers["Accept"] = "application/json"
 

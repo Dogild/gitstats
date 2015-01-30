d587c499dfd32bc8ad9f84265b586c459dbdc076
Alexandre Wilhelm <contact.dogild@gmail.com>
2015-01-29 20:48:33 -0800

    Added test for github_connection

--- gitstats/tests/unit/test_github_connections.py
+++ gitstats/tests/unit/test_github_connections.py
@@ -22,7 +22,7 @@ class GithubConnectionTests(TestCase):
         assert uri == "https://api.github.com/users?per_page=100"
 
     def test_method_add_params_with_param(self):
-        """Test the method _add_params with params, the method adds by default the param per_page=100"""
+        """Test the method _add_params with param, the method adds by default the param per_page=100"""
 
         github_connection = GithubConnection("Dogild")
         params = dict()
@@ -30,3 +30,14 @@ class GithubConnectionTests(TestCase):
         uri = github_connection._add_params(self.uri, params)
 
         assert uri == "https://api.github.com/users?test=32&per_page=100"
+
+    def test_method_add_params_with_params(self):
+        """Test the method _add_params with params, the method adds by default the param per_page=100"""
+
+        github_connection = GithubConnection("Dogild")
+        params = dict()
+        params["test"] = "32"
+        params["nosetest"] = "coucou"
+        uri = github_connection._add_params(self.uri, params)
+
+        assert uri == "https://api.github.com/users?test=32&per_page=100&nosetest=coucou"
